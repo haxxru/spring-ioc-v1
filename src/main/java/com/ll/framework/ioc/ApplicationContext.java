1a2a725b79f4957139c463e208890d8c6602266a
Unordinary <92520481+Unord1nary@users.noreply.github.com>
2026-04-22 17:46:46 +0900

    Feat : t2 Green

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,11 +1,22 @@
 package com.ll.framework.ioc;
 
+import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestPostService;
+
 public class ApplicationContext {
     public ApplicationContext() {
 
     }
 
     public <T> T genBean(String beanName) {
-        return (T) null;
+        if (beanName.equals("testPostRepository")) {
+            return (T) new TestPostRepository();
+        }
+
+        if (beanName.equals("testPostService")) {
+            return (T) new TestPostService(genBean("testPostRepository"));
+        }
+
+        return null;
     }
 }
