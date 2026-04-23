914da0bc090695a906efd5806c53b21d64b6aef7
Unordinary <92520481+Unord1nary@users.noreply.github.com>
2026-04-23 14:14:33 +0900

    Feat : t3 Green

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -3,20 +3,17 @@ package com.ll.framework.ioc;
 import com.ll.domain.testPost.testPost.repository.TestPostRepository;
 import com.ll.domain.testPost.testPost.service.TestPostService;
 
-public class ApplicationContext {
-    public ApplicationContext() {
+import java.util.HashMap;
+import java.util.Map;
 
+public class ApplicationContext {
+    private final Map<String, Object> beans = new HashMap<>();
+
+    public ApplicationContext() {
+        beans.put("testPostService", new TestPostService(new TestPostRepository()));
     }
 
     public <T> T genBean(String beanName) {
-        if (beanName.equals("testPostRepository")) {
-            return (T) new TestPostRepository();
-        }
-
-        if (beanName.equals("testPostService")) {
-            return (T) new TestPostService(genBean("testPostRepository"));
-        }
-
-        return null;
+        return (T) beans.get(beanName);
     }
 }
